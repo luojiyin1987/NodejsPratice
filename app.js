0ec079164752cdeaf376528b592062524b9918da
luojiyin <luojiyin@hotmail.com>
2017-02-19 12:34:09 +0800

    try use Module

---
+++ app.js
@@ -0,0 +1,15 @@
+var debug = require('debug')('http')
+,   http  = require('http')
+,   name  = 'My App';
+
+debug('booting %s', name);
+
+http.createServer(function(req, res){
+    debug(req.method + ' '  + req.url );
+    res.end('hello\n');
+}).listen(3000, function(){
+    debug('listening');
+});
+
+//fake worker of some kind
+require('./worker') ;
