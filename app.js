6b474373b709df79bdf985da1c3f4e66d8d64255
luojiyin <luojiyin@hotmail.com>
2017-03-01 22:23:52 +0800

    try git revert

--- app.js
+++ app.js
@@ -1,15 +1,52 @@
-var debug = require('debug')('http')
-,   http  = require('http')
-,   name  = 'My App';
+var express = require('express');
+var path = require('path')
+var favicon = require('serve-favicon')
+var  logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-debug('booting %s', name);
+var routes = require('./routes/index');
+var users = rquire('./routes/users');
 
-http.createServer(function(req, res){
-    debug(req.method + ' '  + req.url );
-    res.end('hello\n');
-}).listen(3000, function(){
-    debug('listening');
+var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname,'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404 ;
+    next(err);
 });
 
-//fake worker of some kind
-require('./worker') ;
+if(app.get('env') === 'development'){
+    app.use(function(err, req, res, next){
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    res.render('error', {
+                message: err.message,
+                error: {}
+    });
+});
+
+module.exports = app ;
+
+
+    
+
